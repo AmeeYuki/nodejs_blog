cd65850e94079f47ce2facf70927bdab3791f4bf
AmeeYuki <yukinooame@gmail.com>
2023-12-30 23:42:37 +0700

    Route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 //Use public folder
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -27,22 +29,9 @@ app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.set("views", path.join(__dirname, "resources/views"));
-app.get("/", (req, res) => {
-  res.render("home");
-});
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("");
-});
+//Route inits
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
