e7bd9b258da9a22b692883fa3792ac92a2ee277d
AmeeYuki <yukinooame@gmail.com>
2023-12-31 01:41:48 +0700

    add husky pretier lint-staged

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
 //Use public folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Use middleware
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 
 app.use(express.json());
@@ -25,14 +25,14 @@ app.use(express.json());
 // app.use(morgan("combined"));
 
 //Template
-app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route inits
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
